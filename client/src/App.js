d5c582051893e295e82f7f02c67e936fae8d2d68
Guido Queiroz <Takzzg@hotmail.com>
2022-03-11 17:11:54 -0300

    CRA clean-up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Henry Food</h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
